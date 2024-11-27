dabd5c659af395afdd28fdcc316f17d2df2a3cd9
Norens-design <norelei@terpmail.umd.edu>
2024-11-26 20:19:27 -0500

    Add files via upload

---
+++ Tuple_Out_Dice_Game_Consolidation.py
@@ -0,0 +1,16 @@
+# # # "Tuple Out" Dice Game Consolidation Project
+ # # List of items to remember to incorporate in different lines
+# ------------------------------------------------
+import os
+os.path()
+
+# Create at least 2 fictional people
+def greet(name):
+  """This function is to introduce a player."""
+  print("Hello, " + name + "Sophia! You will roll first.")
+
+def greet(name):
+  """This function is to introduce a player."""
+  print("Hello, " + name + "Fatima! You will roll after")
+  print("each turn Sophia takes.")  
+
